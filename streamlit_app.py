238570b2463e06c254d5162d0fb3eb0a4c888294
moonbuggy42 <mihin0011@gmail.com>
2026-05-09 14:48:37 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,5 @@ with st.expander("Data"):
   y
 with st.expander("Data Visualization"):
   fig1 = px.scatter(df, x="bill_length_mm", y="body_mass_g", color="species", hover_data=["island"])
+  fig1.update_layout(xaxis_title="Bill Length (mm)", yaxis_title="Body Mass (g)")
   st.plotly_chart(fig1)
