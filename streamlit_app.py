4725af914334da54b5b434028d7fd36478d8a624
moonbuggy42 <mihin0011@gmail.com>
2026-05-09 15:22:58 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,8 @@ with st.sidebar:
           'gender': gender}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
-# input_df
-input_penguins
+with st.expander('Input Features'):
+  st.write("**Input Penguin**")
+  input_df
+  st.write("**Conbined Penguins Data**")
+  input_penguins
