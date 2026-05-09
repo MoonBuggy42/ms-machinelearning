cfd1b0a8f03c4d83913cf81c80769474ee68805f
moonbuggy42 <mihin0011@gmail.com>
2026-05-09 15:40:02 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,12 @@ with st.expander("Data"):
   df
 
   st.write('**X**')
-  X = df.drop("species", axis=1)
-  X
+  X_raw = df.drop("species", axis=1)
+  X_raw
 
   st.write("**y**")
-  y=df.species
-  y
+  y_raw =df.species
+  y_raw
 # Data Visuals
 with st.expander("Data Visualization"):
   fig1 = px.scatter(df, x="bill_length_mm", y="body_mass_g", color="species", hover_data=["island"])
@@ -41,13 +41,19 @@ with st.sidebar:
           'body_mass_g': body_mass_g, 
           'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=0)
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
 
-  # Encode 
-  encode = ['island', 'sex']
-  df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-  input_row = df_penguins[:1]
+# Encode X
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+input_row = df_penguins[:1]
 
+# Encode y
+target_mapper = {'Adelie': 0, 
+                 'Chinstrap': 1, 
+                 'Gentoo': 2}
+def target_encode(val):
+  return target_mapper[val]
 
 with st.expander('Input Features'):
   st.write("**Input Penguin**")
