59e81fb4f410a8bddab5ba3926c83f64f7404543
moonbuggy42 <mihin0011@gmail.com>
2026-05-09 15:04:15 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,6 @@ with st.expander("Data Visualization"):
 # Additional Data Prep
 with st.sidebar:
   st.header("Input Features")
-  island = st.selectbox("Island", list('Biscoe', 'Dream', 'Torgersen'))
-  gender = st.selectbox("Gender", list('male', 'female'))
+  island = st.selectbox("Island", ('Biscoe', 'Dream', 'Torgersen'))
+  gender = st.selectbox("Gender", ('male', 'female'))
   bill_length_mm = st.slider("Bill Length (mm)", 32.1, 59.6, 43.9)
